515b9051003e87ef7c83bf1456b17e1bc811389f
Allyson Jeronimo <allysonjeronimo@gmail.com>
2025-05-15 19:49:34 -0300

    Player animations

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const SPEED = 60.0
 const JUMP_VELOCITY = -300.0
@@ -8,7 +9,7 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+		
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -21,4 +22,18 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	update_animations(direction)
 	move_and_slide()
+
+func update_animations(direction):
+	if is_on_floor():
+		if direction < 0.0:
+			animated_sprite_2d.play("walk")	
+			animated_sprite_2d.flip_h = true
+		elif direction > 0.0:
+			animated_sprite_2d.play("walk")
+			animated_sprite_2d.flip_h = false
+		else:
+			animated_sprite_2d.play("idle")
+	else:
+		animated_sprite_2d.play("jump")
